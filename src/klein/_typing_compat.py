42abb27dfb15ad5a433ab12ddf2c2723732eed77
Glyph <code@glyph.im>
2025-03-31 22:42:04 -0700

    drop py37 syntax target, unnecessary import conditional

--- src/klein/_typing_compat.py
+++ src/klein/_typing_compat.py
@@ -5,19 +5,13 @@ to avoid repeating conditional import logic.
 """
 
 import sys
-
-
-if sys.version_info > (3, 8):
-    from typing import Protocol
-else:
-    from typing_extensions import Protocol
+from typing import Protocol
 
 
 if sys.version_info > (3, 10):
     from typing import Concatenate, ParamSpec
 else:
     from typing_extensions import Concatenate, ParamSpec
-
 
 __all__ = [
     "Protocol",
